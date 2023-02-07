165851c8fdcaebcd32f34b6976f75135232468e2
david <davmwa90@gmail.com>
2023-02-08 00:38:23 +0300

    completed control-flow-loops lab

--- looping.rb
+++ looping.rb
@@ -1,10 +1,16 @@
 def happy_new_year
   # your code here
+number = 10 
+until number == 0
+  puts number
+  number -= 1
+end
+puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
+if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
   elsif num % 3 == 0
     "Fizz"
@@ -17,8 +23,15 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).map { |number|
+puts (fizzbuzz(number)) }
 end
 
 def reverse_string(str)
-  # your code here
+  split_string = str.split("")
+  reversed_string = []
+  str.size.times {reversed_string << split_string.pop}
+  reversed_string.join
+
 end
+reverse_string("david")
